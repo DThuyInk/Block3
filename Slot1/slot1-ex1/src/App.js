3569271900eebb8e9c039b1b4741825c9802259a
DThuyInk <trannguyendangthuy120701@gmail.com>
2025-08-11 10:00:57 +0700

    slot1-ex1

---
+++ Slot1/slot1-ex1/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  const myName = "Thuy"; // Thay "Your Name" bằng tên của bạn nếu muốn
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>My name is, {myName}</p>
+    </div>
+  );
+}
+
+export default App;
